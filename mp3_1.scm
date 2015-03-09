61b53f793d6aa1b42707f8e35495705548027bf7
Sammi Liu <sliu19@illinois.edu>
2015-03-09 01:23:48 -0500

    TestCase 1-5

--- mp3_1.scm
+++ mp3_1.scm
@@ -47,7 +47,7 @@
 
 (define-datatype proc proc?
     (procedure
-      (bvar symbol?)
+      (bvar list?)
       (body expression?)
       (env environment?)))
   
@@ -63,6 +63,7 @@
    (proc-bodies (list-of expression?))
    (next-env environment?)))
 
+
 (define apply-env
   (lambda (search-sym env)
     (cases environment env
@@ -71,7 +72,7 @@
       (extend-env (bvar bval next-env)
                   (if (eqv? search-sym bvar)
                       bval
-                      (apply-env next-env search-sym)))
+                      (apply-env  search-sym next-env)))
       (extend-env-rec* (p-names b-vars p-bodies next-env)
                        (cond 
                          ((location search-sym p-names)
@@ -81,7 +82,7 @@
                                  (list-ref b-vars n)
                                  (list-ref p-bodies n)
                                  env))))
-                         (else (apply-env next-env search-sym)))))))
+                         (else (apply-env search-sym  next-env)))))))
 
 (define location
     (lambda (sym syms)
@@ -184,6 +185,35 @@
                   (inner-loop (cdr sto) (+ n 1)))))))
          (inner-loop the-store 0))))
 
+(define expval->num
+    (lambda (v)
+      (cases expval v
+	(num-val (num) num)
+	(else (expval-extractor-error 'num v)))))
+
+  (define expval->bool
+    (lambda (v)
+      (cases expval v
+	(bool-val (bool) bool)
+	(else (expval-extractor-error 'bool v)))))
+
+  (define expval->proc
+    (lambda (v)
+      (cases expval v
+	(proc-val (proc) proc)
+	(else (expval-extractor-error 'proc v)))))
+
+  (define expval->ref
+    (lambda (v)
+      (cases expval v
+	(ref-val (ref) ref)
+	(else (expval-extractor-error 'reference v)))))
+
+  (define expval-extractor-error
+    (lambda (variant value)
+      (eopl:error 'expval-extractors "Looking for a ~s, found ~s"
+	variant value)))
+
 
 ;=====================================Value-of========================================
 (define value-of
@@ -191,23 +221,22 @@
     (cond
       [(number? exp) exp]
       [(expression? exp)
+       (display "This is debug for value-of expression")
        (cases expression exp
-         (num-exp (number) number)
+         (num-exp (number) (num-val number))
          (var-exp (var) (apply-env var env))
          (let-exp (var-list exp1-list exp2) (value-of-let var-list exp1-list exp2 env))
          (letrec-exp (var-list exp1-list body)(value-of-letrec) )
-         (arith-exp(arith-op exp1 exp2) (value-of-arith-exp arith-op exp1 exp2 env))
-         
-         (set-exp (var value) (value-of-set var value env))
+         (proc-exp(var-list exp) (proc-val (procedure var-list exp env)))
+         (exp-exp(rator rand-list) (value-of-exp rator rand-list env))
          (newRef-exp (exp) (ref-val (newref exp)))
+         (set-exp (var value) (value-of-set var value env))
          (begin-exp (exp1 exp2-list) (value-of-begin exp1 exp2-list env))
+         
+         (arith-exp(arith-op exp1 exp2) (value-of-arith-exp arith-op exp1 exp2 env))
          (else exp))]
-      [(expval? exp)
-       (cases expval exp
-         (num-val(value) value)
-         (bool-val(bool) bool)
-         (proc-val(proc) proc)
-         (ref-val(ref) (deref ref)))])))
+      [(expval? exp) exp])))
+  
 
 (define value-of-letrec
   (lambda(p-names b-vars p-bodies letrec-body env)
@@ -226,15 +255,27 @@
     (if (null? exp2-list)
         (value-of exp1 env)
         (cond
-          [(equal? arith-op '+) (value-of-arith-exp arith-op (num-exp (+ (value-of exp1 env) (value-of (car exp2-list) env))) (cdr exp2-list) env)]
-          [(equal? arith-op '-) (value-of-arith-exp arith-op (num-exp (- (value-of exp1 env) (value-of (car exp2-list) env))) (cdr exp2-list) env)]
-          [(equal? arith-op '*) (value-of-arith-exp arith-op (num-exp (* (value-of exp1 env) (value-of (car exp2-list) env))) (cdr exp2-list) env)]
-          [(equal? arith-op '/) (value-of-arith-exp arith-op (num-exp (/ (value-of exp1 env) (value-of (car exp2-list) env))) (cdr exp2-list) env)]
+          [(equal? arith-op '+) (value-of-arith-exp arith-op (num-exp (+ (expval->num (value-of exp1 env)) (expval->num (value-of (car exp2-list) env)))) (cdr exp2-list) env)]
+          [(equal? arith-op '-) (value-of-arith-exp arith-op (num-exp (- (expval->num (value-of exp1 env)) (expval->num (value-of (car exp2-list) env)))) (cdr exp2-list) env)]
+          [(equal? arith-op '*) (value-of-arith-exp arith-op (num-exp (* (expval->num (value-of exp1 env)) (expval->num (value-of (car exp2-list) env)))) (cdr exp2-list) env)]
+          [(equal? arith-op '/) (value-of-arith-exp arith-op (num-exp (/ (expval->num (value-of exp1 env)) (expval->num (value-of (car exp2-list) env)))) (cdr exp2-list) env)]
           [else display "no match"]))))
 
 (define value-of-let
   (lambda (var-list exp1-list exp2 env)
      (value-of exp2 (add-env var-list exp1-list env))))
+
+(define value-of-exp
+  (lambda (rator rand-list env)
+    (let ((proc (expval->proc (value-of rator env)))
+          (arg (value-of-arg rand-list env)))
+      (apply-procedure proc arg))))
+       
+(define value-of-arg
+  (lambda (arg-list env)
+    (if (null? (cdr arg-list))
+        (list (value-of (car arg-list) env))
+        (append (list (value-of (car arg-list) env)) (value-of-arg (cdr arg-list) env)))))
 
 (define value-of-begin
   (lambda (exp1 exps env)
@@ -253,13 +294,24 @@
         (extend-env (car var-list) (value-of (car exp1-list) env) env)
         (extend-env (car var-list) (value-of (car exp1-list) env) (add-env (cdr var-list) (cdr exp1-list) env)))))
 
-
+(define apply-procedure
+  (lambda (proc1 arg)
+    (cases proc proc1
+      (procedure (var body saved-env)
+                 (let ((r arg))
+                   (let ((new-env (add-env var r saved-env)))
+                     (value-of body new-env)))))))
 
 ;==============================Wrap Func=================================
 (define static-interpreter
   (lambda (exp)
     (initialize-store!)
-    (value-of (value-of (scan&parse exp) (empty-env)) (empty-env))))
+    (let ([result (value-of (scan&parse exp) (empty-env))])
+      (cases expval result
+        (ref-val(ref) (expval->num (deref ref)))
+        (num-val(value) value)
+        (bool-val(bool) bool)
+        (proc-val(proc)  proc)))))
 
 
 ;=====================================Test========================================
@@ -273,11 +325,17 @@
 (trace add-env)
 (trace scan&parse)
 (trace value-of-set)
+(trace value-of-exp)
+(trace apply-procedure)
 ;(trace the-store)
 ;(display (scan&parse ">(3,+(1,2))"))
 ;(display (scan&parse "let x = 1 in let f = proc (y) +(x, y) in let x = 2 in (f 5)"))
 ;(display (scan&parse "let x = 1 in let f = proc (y) +(x, y) in let x = 2 in (f 5)"))
 ;(display (scan&parse "letrec ill = proc (x) (ill x) in let f = proc (y) 5 in (f (ill 2))"))
 
-(static-interpreter "let x = newref(1) in begin set x 2;x end")
-;(static-interpreter "let x = 5 y = 3 in begin +(x,y) end")
+;(static-interpreter "let x = newref(1) in begin set x 2;x end")
+;(static-interpreter "let x = let y = newref(1) in begin set y 2;y end in x")
+;(static-interpreter "let x = newref(1) in let f= proc (y) set y 2 in begin (f x); x end")
+;(static-interpreter "let f=proc(x y) +(x,y) g=proc(x y z) +(x,y,z) in (f (g 1 2 3)1)")
+;(static-interpreter "let f = proc(x) proc(y) +(x,y) in let g= proc(x)proc(y)proc(z) +(x,y,z) in ((f (((g 1)2)3))1)")
+(static-interpreter "let f = newref (proc (x y) +(x,y)) in begin set f proc (x y) -(x,y); (f 5 1) end")
