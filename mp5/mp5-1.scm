a5de0de46fdce7689a1aa5f95b49a32503e55324
yigh <yizhou12345@gmail.com>
2015-04-22 19:59:24 -0500

    wokring set

--- mp5/mp5-1.scm
+++ mp5/mp5-1.scm
@@ -7,7 +7,122 @@
       (fn (car list)
           (reduce fn init (cdr list) ))))
 
+;==== References
+(define instrument-newref (make-parameter #f))
+;; the-store: a Scheme variable containing the current state of the
+;; store.  Initially set to a dummy variable.
+(define the-store 'uninitialized)
 
+(define get-reference
+  (lambda (ref-type)
+    (cadr ref-type)))
+
+;; empty-store : () -> Sto
+;; Page: 111
+(define empty-store
+  (lambda () '()))
+
+;; initialize-store! : () -> Sto
+;; usage: (initialize-store!) sets the-store to the empty-store
+;; Page 111
+(define initialize-store!
+  (lambda ()
+    (set! the-store (empty-store))))
+
+;; get-store : () -> Sto
+;; Page: 111
+;; This is obsolete.  Replaced by get-store-as-list below
+(define get-store
+  (lambda () the-store))
+
+(define newref
+  (lambda (x)
+    (list "ref-type" (newref-helper x))))
+
+;; newref : ExpVal -> Ref
+;; Page: 111
+(define newref-helper
+  (lambda (val)
+    (let ((next-ref (length the-store)))
+      (set! the-store
+            (append the-store (list val)))
+      (when (instrument-newref)
+        (eopl:printf 
+         "newref: allocating location ~s with initial contents ~s~%"
+         next-ref val))                     
+      next-ref)))                     
+
+(define deref
+  (lambda (reftype)
+    (deref-helper (cadr reftype))))
+
+(define deref-loc
+  (lambda (loc)
+    (deref-helper loc)))
+
+(define deref-helper
+  (lambda (ref)
+    ; Control reaches here after loc/val is extracted from environment. 
+    ; Only ints are valid locations, and the int must be a valid location in the store.
+    ; 0-index addressing.
+    (cond ((number? ref) 
+            (if (or (>= ref (length the-store))
+                    (equal? 0 (length the-store)))
+                'undefined 
+                (list-ref the-store ref)))
+          (else 'undefined))))
+
+(define get-store-as-list
+     (lambda ()
+       (letrec
+         ((inner-loop
+            ;; convert sto to list as if its car was location n
+            (lambda (sto n)
+              (if (null? sto)
+                '()
+                (cons
+                  (list n (car sto))
+                  (inner-loop (cdr sto) (+ n 1)))))))
+         (inner-loop the-store 0))))
+
+(define setref
+  (lambda (ref-type val)
+    (cond ((equal? 'undefined val) val)
+          ((equal? 'undefined ref-type) ref-type)
+          ((is-reference? ref-type) (begin 
+                                      (setref-helper (cadr ref-type) val)
+                                      'undefined))
+          (else (begin 
+                  (setref-helper ref-type val)
+                  'undefined)))))
+
+(define setref-helper
+  (lambda (ref val)
+    (set! the-store
+          (letrec
+              ((setref-inner
+                ;; returns a list like store1, except that position ref1
+                ;; contains val. 
+                (lambda (store1 ref1)
+                  (cond
+                    ((null? store1)
+                     'undefined)
+                    ((zero? ref1)
+                     (cons val (cdr store1)))
+                    (else
+                     (cons
+                      (car store1)
+                      (setref-inner
+                       (cdr store1) (- ref1 1))))))))
+            (setref-inner the-store ref)))))
+
+
+(define is-reference? 
+  (lambda (arg)
+    (cond ((and (list? arg) (equal? 2 (length arg)) (equal? "ref-type" (car arg))) #t)
+          (else #f))))
+
+(initialize-store!)
 ;=================================Spec&Grammar=====================================
 
 ;;scanner
@@ -44,7 +159,7 @@
     (expression ("true") true-exp)
     (expression ("false") false-exp)
     (expression (obj-exp (arbno "."identifier)) object)
-    (obj-exp ("begin" expression (arbno expression ";") "end")begin-exp)
+    (obj-exp ("begin" (arbno expression ";") "end")begin-exp)
     (obj-exp ("if" expression "then" expression "else" expression "end")if-exp)
     (obj-exp ("let" (arbno identifier "=" expression) "in" expression "end") let-exp) 
     (obj-exp ("letrec" (arbno identifier "=" expression) "in" expression "end") letrec-exp)
@@ -69,31 +184,57 @@
 
 ;Env: (key （exp protection）)   ;;protection 1:public   0:non-member     -1:protected
 (define extend-env
-  (lambda (key value env)
-      (cond ((null? env) (list (list key value)))
-            ((equal? (caar env) key) (append (list (list key value)) (cdr env)))
-            (else (append (list (car env)) (extend-env key value (cdr env) ))))))
+  (lambda (key value possibly-env-ref)
+    (let* ([env (if (is-reference? possibly-env-ref)
+                    (deref possibly-env-ref)
+                    possibly-env-ref)])
+      (cond ((null? env) (begin 
+                           (setref possibly-env-ref (list (list key value)))
+                           possibly-env-ref))
+            ((equal? (caar env) key) (begin 
+                                       (setref possibly-env-ref (append (list (list key value)) (cdr (deref possibly-env-ref))))
+                                       possibly-env-ref))
+            (else (append (list (car env)) (extend-env key value (cdr env) )))))))
 
 (define apply-helper
-  (lambda (env key)
-    (cond ((null? env) 'undefined)
-          ((equal? key (caar env))
-           ;(if (expression? (cadar env))
-                (cadar env))
-               ; (car env)))
-          (else (apply-helper (cdr env) key)))))
+  (lambda (possibly-env-ref key)
+    (let* ([env (if (is-reference? possibly-env-ref)
+                    (deref possibly-env-ref)
+                    possibly-env-ref)])
+      (cond ((null? env) 'undefined)
+            ((equal? key (caar env))
+             ;(if (expression? (cadar env))
+             (cadar env))
+            ; (car env)))
+            (else (apply-helper (cdr env) key))))))
  
 (define apply-env
+  (lambda (key possibly-env-ref)
+    (let* ([env (if (is-reference? possibly-env-ref)
+                    (deref possibly-env-ref)
+                    possibly-env-ref)]
+           [result (apply-helper env key)])
+      (if (equal? 'undefined result)
+          'undefined
+          (if (expression? (car result))
+              (value-of (car result) env)
+              (car result))))))
+
+;Bug? How is this getting the right value?
+(define apply-env-protection
   (lambda (key env)
     (let [(result (apply-helper env key))]
-      (if (expression? (car result))
-          (value-of (car result) env)
-          (car result)))))
+      (if (equal? 'undefined result)
+          'undefined
+          (cadr result)))))
+;          (if (expression? (car result))
+;              (value-of (car result) env)
+;              (car result))))))
    
 
 (define emptyObject
   (lambda ()
-    (list (empty-env) '())))
+    (list (newref (empty-env)) '())))
 
 (define subClass
   (lambda (self super)
@@ -121,24 +262,99 @@
     (cases member exp
       (mem(exp1 id exp) exp))))
 
-
-
 (define obj-env
   (lambda (obj)
     (car obj)))
 
+(define env-obj-is-protected
+  (lambda (id env)
+    (let ([protection-type (apply-env-protection id env)])
+      (cond 
+        ((equal? protection-type 'undefined) 'undefined)
+        ((equal? protection-type -1) #t)
+        (else #f)))))
+(define env-obj-is-public
+  (lambda (id env)
+    (let ([protection-type (apply-env-protection id env)])
+      (cond 
+        ((equal? protection-type 'undefined) 'undefined)
+        ((equal? protection-type 1) #t)
+        (else #f)))))
+
+
 ;==============================Value-of Functions===================================
 ; Helper to find the environment corresponding to the identifier list (id-list)
-(define get-set-env 
-  (lambda (obj id-list)
+(define set-env-helper 
+  (lambda (obj id-list val-exp bt-self-or-super env)
     (if (equal? 1 (length id-list))
-        ; the list of IDs is length 1 => find the identifier in obj's env
-        (apply-env (car id-list) (obj-env obj))
-
+        (let ([val-from-env (apply-env (car id-list) (obj-env obj))])
+          ; does the id even exist
+          (if (equal? val-from-env 'undefined)
+              'undefined
+              
+              ; handle protection cases
+              (if (env-obj-is-public (car id-list) (obj-env obj))
+                  ; public case : extend environment
+                  (extend-env (car id-list) (list val-exp 1) (obj-env obj))
+                  ; protected case : is accessible if called from self/super
+                  (if bt-self-or-super
+                      (extend-env (car id-list) (list val-exp -1) (obj-env obj))
+                      ; scope not allowed
+                      'undefined))))
+                  
+                   
         ; the list of IDs is length >1 => consume car of list = some member variable of obj
-        (let ([recurse-obj (apply-env (car id-list) (obj-env obj))]
-              [remainder-list (cdr id-list)])
-          (get-set-env recurse-obj remainder-list)))))
+        (let* ([recurse-obj (apply-env (car id-list) (obj-env obj))]
+               [is-public (env-obj-is-public (car id-list) (obj-env obj))]
+               [remainder-list (cdr id-list)])
+          ;; id list will never be a self or super so there will not be valid protected types
+          (if is-public
+              (set-env-helper recurse-obj remainder-list val-exp #f env)
+              ; is protected
+              (if bt-self-or-super
+                  (set-env-helper recurse-obj remainder-list val-exp #f env)
+                  'undefined
+              ))))))
+
+(define (list-last-element l)
+  (cond ((null? (cdr l)) (car l))
+        (else (list-last-element (cdr l)))))
+
+(define begin-helper
+  (lambda (exp-list env)
+    (let ([first-exp (car exp-list)]
+          [rest (cdr exp-list)])
+      (if (null? rest)
+          (value-of first-exp env)
+          (begin
+            (value-of first-exp env)
+            (begin-helper rest env))))))
+
+(define (zip . xss) (apply map list xss))
+(define add-id-to-object-pairs 
+  (lambda (id-exp-pairs env)
+    (if (null? id-exp-pairs) 
+        env 
+        (let* ([pair (car id-exp-pairs)]
+               [rest (cdr id-exp-pairs)]
+               [id (car pair)]
+               [expr (cadr pair)]
+               [is-obj-creation (if (expression? expr)
+                                    (cases expression expr
+                                      (object (exp id-list) 
+                                              (cases obj-exp exp
+                                                (extend-exp (exp dec-lis) #t)
+                                                (else #f)))
+                                      (else #f))
+                                    #f)]
+               [existing-obj (apply-env id env)]
+               [extended-obj (if (equal? existing-obj 'undefined)
+                                 'undefined
+                                 (list (append existing-obj (list id)) 0))]
+               [extended-env (if is-obj-creation
+                                 (extend-env id extended-obj env)
+                                 env)])
+          extended-env))))
 
 (define value-of
   (lambda (exp  env)
@@ -147,7 +363,11 @@
        (cases obj-exp exp
          
          (let-exp (id-list exp-list body)
-                  (value-of body (add-env id-list exp-list env)))
+                  (let* ([extended-env (add-env id-list exp-list env)]
+                        [id-exp-pairs (zip id-list exp-list)]
+                        [env-obj-with-ids (add-id-to-object-pairs id-exp-pairs extended-env)])
+                    (value-of body env-obj-with-ids)))
+         
          (letrec-exp(id-list exp-list body)
                   (value-of body (add-env-rec id-list exp-list env)))
          
@@ -157,6 +377,13 @@
                     (subClass (add-mem mem-list env) (value-of exp env)))
          ;(self-exp ())
          (var-exp (var) (apply-env var env))
+         
+         ;begin
+         (begin-exp (exp-list)
+                    (if (equal? 0 (length exp-list))
+                        'undefined
+                        (begin-helper exp-list env)))
+         
          (else  exp))
        ]
       [(expression? exp)
@@ -174,28 +401,45 @@
          ; proc-exp
          ; looks like you cannot pass arguments to proc
          ; procs return body exp for dynamic dispatch
-         (proc-exp (arg-list body) body)
+         (proc-exp (arg-list body) (arg-list body))
     
          ; set-exp
          ; val exp also dynamic binding, so we do not evaluate here
          (set-exp (exp1 val-exp)
-                  (cases expression exp1
-                    (object (obj-exp id-list) 
-                            (let* ([obj (value-of obj-exp env)]
-                                   [target-env (get-set-env obj id-list)])
-                              (if (equal? target-env 'undefined)
-                                  ; Set target not found
-                                  'undefined
-                                  
-                                  ; Set target was found
-                                  (extend-env target-env val-exp))
-                              ))
-                    (else 'undefined)))
-                  
-    
-         ; object)
+                  (cond  
+                    ((expression? exp1)
+                     (cases expression exp1
+                       
+                       
+                       (object (object-exp id-list) 
+                               ; For handling protection classes, we can access protected members 
+                               ; only from super and self keywords
+                               (cases obj-exp object-exp
+                                 ;; TODO recursive searching
+                                 (self-exp ()(let* ([obj (value-of object-exp env)])
+                                               (set-env-helper obj id-list val-exp #t env)))
+                                 
+                                 (super-exp ()(let* ([obj (value-of object-exp env)])
+                                                (set-env-helper obj id-list val-exp #t env)))
+                                 
+                                 (else (let* ([obj (value-of object-exp env)])
+                                         (set-env-helper obj id-list val-exp #f env)))))
+                       
+                       
+                       ; None of other expressions can give an identifier
+                       (else 'undefined)))
+                    
+                    
+                    
+                    ; Setting Identifiers shouldnt even be allowed by the grammar.
+                    ((obj-exp? exp1)
+                     (cases obj-exp exp1
+                       (var-exp (id) 
+                                ; protection class?
+                                (extend-env id val-exp env))
+                       (else 'undefined)))
+                    ))
          
-        
          (compare-equ-exp (exp1 exp2)
                       (= (value-of exp1 env) (value-of exp2 env)))
         
@@ -230,7 +474,7 @@
 (define add-mem
   (lambda (mem-list env)
     (if (null? mem-list)
-        (empty-env)
+        (newref (empty-env))
         (extend-env (member->id (value-of (car mem-list) env))(list (member->exp (value-of (car mem-list) env))(member->pub (value-of (car mem-list) env))) (add-mem (cdr mem-list) env)))))
  
 (define value-of-object
@@ -243,7 +487,7 @@
 ;===============================Object-interpreter===================================
 (define object-interpreter
   (lambda (exp)
-    (value-of (scan&parse exp) (empty-env))))
+    (value-of (scan&parse exp) (newref (empty-env)))))
 
 
 (trace object-interpreter)
@@ -260,7 +504,10 @@
 
 ;(object-interpreter "extend EmptyObj with public a =3;  protected b = a; public c = 1;")
 ;(object-interpreter "letrec a =b b = 3 c = a in +(a,b,c) end");9
-(object-interpreter "let ob = extend EmptyObj with public x =1; in ob.x end");1
+;(object-interpreter "let ob = extend EmptyObj with public x =1; in ob.x end");1
 ;(scan&parse "a.b.c.x")
 
-(object-interpreter "let a = extend EmptyObj with public b = extend EmptyObj with public c = extend EmptyObj with public x = 5 ; ; ; in a.b.c.x end");1
+;(object-interpreter "let a = extend EmptyObj with public b = extend EmptyObj with public c = extend EmptyObj with public x = 5 ; ; ; in a.b.c.x end");1
+
+
+;(object-interpreter "let ob = extend EmptyObj with public x =1; in begin (set ob.x 2) ; ob.x; end end")
